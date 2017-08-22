434343767c54385b8761991c18a8044b42816fbc
ᦖєgιт <support@legitsoulja.info>
2017-08-21 21:51:43 -0400

    Update DocWriter.php

--- DocWriter/DocWriter.php
+++ DocWriter/DocWriter.php
@@ -3,12 +3,24 @@
 namespace Document;
 class Doc extends DocWriter {
     
+    /*
+    \| Cleans HTML code
+    \| @var array $site Stores html, head, and body
+    */
     protected $site = [null, null, null];
     
+    /*
+    \| Doc constructor
+    */
     function __construct(){
         parent::createDoc($this->site[0],$this->site[1], $this->site[2]);
     }
-
+    
+    /*
+    \| Cleans HTML code
+    \| @param string $html | HTML string
+    \| @return string
+    */
     private function clean_html($html)
     {
         $dom = new \DOMDocument();
@@ -41,20 +53,37 @@ class Doc extends DocWriter {
         return false;
     }
 
+    /*
+    \| Render/Output HTML
+    \| @output bool True to output HTML, false to return string
+    \| @return string
+    */
     public function render($output = false){
         $html = $this->clean_html($this->html()->toHTML());
         if(!$output) return $html;
         echo $html;
     }
     
+    /*
+    \| HTML Element
+    \| @return DocElement
+    */
     public function html(){
         return $this->site[0];
     }
     
+    /*
+    \| HEAD Element
+    \| @return DocElement
+    */
     public function head(){
         return $this->site[1];
     }
     
+    /*
+    \| BODY Element
+    \| @return DocElement
+    */
     public function body() {
         return $this->site[2];
     }
@@ -63,10 +92,26 @@ class Doc extends DocWriter {
 
 class DocWriter
 {
+    
+    /*
+    \| Create tag
+    \| @param string $tagname The tagname of your element
+    \| @param array $attributes The attributes of your element
+    \| @param string $innerHTML The innerHTML content of your element
+    \| @return DocElement
+    */
     static function createTag($tagname, $attributes = array(), $innerHTML = null)
     {
         return new DocElement($tagname, $attributes, $innerHTML);
     }
+    
+    /*
+    \| Create document
+    \| @param string $html Variable Reference
+    \| @param string $head Variable Reference
+    \| @param string $body Variable Reference
+    \| @return $html, $head, $hody
+    */
     static function createDoc(&$html, &$head, &$body)
     {
         $head = new DocElement("head");
@@ -77,11 +122,34 @@ class DocWriter
 
 class DocElement
 {
+    
+    /*
+    \| $var string | Element Tag Name
+    */
     private $tagname;
+    
+    /*
+    \| $var array | Element attributes
+    */
     private $attributes;
+    
+    /*
+    \| $var string | Element inner HTML
+    */
     private $innerHTML;
+    
+    /*
+    \| $var array | Child elements of this element
+    */
     private $elements = array();
     
+    /*
+    \| DocElement Contructor
+    \| @param string $a | Element Tag Name
+    \| @param array $b | Element Attributes
+    \| @param string $c | Element InnerHTML
+    \| @return DocElement
+    */
     function __construct($a, $b = array(), $c = null)
     {
         $this->tagname    = $a;
@@ -89,6 +157,14 @@ class DocElement
         $this->innerHTML  = $c;
     }
     
+    /*
+    \| Create child tag in this element
+    \| @param string $tagname | Element Tag Name
+    \| @param array $attributes | Element Attributes
+    \| @param string $innerHTML | Element Inner HTML
+    \| @param string $element | Variable Reference (Returns created tag)
+    \| @return DocElement
+    */
     public function createTag($tagname, $attributes = array(), $innerHTML = null, &$element = null) {
         $element = new DocElement($tagname, $attributes, $innerHTML);
         $this->addChild($element);
@@ -99,16 +175,30 @@ class DocElement
         unset($this->tagname, $this->attributes, $this->innerHTML, $this->elements);
     }
     
+    /*
+    \| Convert Element to HTML
+    \| @return string
+    */
     function __toString()
     {
         return $this->toHTML();
     }
     
+    /*
+    \| Append HTML content
+    \| @param string $a | InnerHTML content
+    \| @return void
+    */
     public function appendHTML($a){
         $this->innerHTML .= $a;
     }
     
-    function toHTML($output = false, $ignoreWarning = false)
+    /*
+    \| Convert Element to HTML
+    \| @param bool $output | True will output the html, false will return the html
+    \| @return string
+    */
+    function toHTML($output = false)
     {
         $attributes = $this->renderAttributes();
         $html       = $this->innerHTML;
@@ -119,6 +209,11 @@ class DocElement
         echo $render;
     }
     
+    /*
+    \| Add child (DocElement) to this Element
+    \| @param DocElement $a 
+    \| @return DocElement | Returns $this
+    */
     function addChild($a)
     {
         $args = func_get_args();
@@ -136,12 +231,21 @@ class DocElement
         return $this;
     }
     
+    /*
+    \| Set Element Attribute
+    \| @param string $name | Name of your attribute
+    \| @param string $value | Your attribute value
+    \| @return DocElement | Returns $this
+    */
     function attr($name, $value)
     {
         $this->attributes[$name] = $value;
         return $this;
     }
     
+    /*
+    \| Renders Element Child Elements
+    */
     private function renderElements()
     {
         $a = "";
@@ -151,6 +255,9 @@ class DocElement
         return $a;
     }
     
+    /*
+    \| Renders Element Attributes
+    */
     private function renderAttributes()
     {
         $a = "";
